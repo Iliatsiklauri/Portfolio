322166a14170325670dbca989e7f1bd95081d37c
ilia tsiklauri <tsiklauri.ilia26@gmail.com>
2024-08-07 20:23:57 +0400

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
 import type { Metadata } from 'next';
 import { Jost } from 'next/font/google';
 import './globals.css';
-import Head from 'next/head';
 
 const inter = Jost({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Ilia Tsiklauri',
   description: 'Portfolio to showcase my work',
+  icons: {
+    icon: '/icons/favicon.ico',
+  },
 };
 
 export default function RootLayout({
@@ -17,9 +19,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
